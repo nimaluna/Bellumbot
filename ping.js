885825965a70e70180d22f06e99f1e8c1a08d1a3
Luna <39229397+nimaluna@users.noreply.github.com>
2018-05-14 10:31:04 -0700

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,14 +1,41 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const commando = require('discord.js-commando');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+class BellumPingCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            group: 'random',
+            memberName: 'ping',
+            description: 'Checks the bot\'s ping to the Discord server.',
+            throttling: {
+                usages: 5,
+                duration: 10
+            }
+        });
+    }
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+    async run(msg) {
+        var phrases = [
+            "Whoa! What's this?",
+            "Hey hey, slow down!!",
+            "YIKES! That was too close!",
+            "Im'm gonna one up you!",
+            "Just in time...",
+            "***Wheeze***?"
+        ]
+        var wphrase = phrases[Math.floor(Math.random() * phrases.length)];
 
-client.login('token');
+        if(!msg.editable) {
+            const pingMsg = await msg.say('Pinging...');
+            return pingMsg.edit(
+                wphrase + ` \n:ping_pong: | Pong! The message round-trip took` + `**` + ` ${pingMsg.createdTimestamp - msg.createdTimestamp}ms.` + `**
+            `);
+        } else {
+            await msg.edit('Pinging...');
+            return msg.edit(
+            wphrase + ` \n:ping_pong: | Pong! The message round-trip took` + `**` + `${msg.editedTimestamp - msg.createdTimestamp}ms`+ `**
+            `);
+        }
+    }
+};
+module.exports = BellumPingCommand;
